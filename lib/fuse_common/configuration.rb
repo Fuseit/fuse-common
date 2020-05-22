551e114c8d7ac505f0c8aebd7fa3048cd80a9c38
Schalk Pienaar <schalk.pienaar@fuseuniversal.com>
2020-05-22 13:54:53 +0200

    fix FUSE-2552 add missing configuration variable required by current setup

--- lib/fuse_common/configuration.rb
+++ lib/fuse_common/configuration.rb
@@ -1,11 +1,12 @@
 module FuseCommon
   class Configuration
-    attr_accessor :airbrake_project_id, :airbrake_project_key, :airbrake_environment_name
+    attr_accessor :airbrake_project_id, :airbrake_project_key, :airbrake_environment_name, :rails_env
 
     def initialize
       @airbrake_project_id = nil
       @airbrake_project_key = nil
       @airbrake_environment_name = Rails.env
+      @rails_env = Rails.env
     end
   end
 end
